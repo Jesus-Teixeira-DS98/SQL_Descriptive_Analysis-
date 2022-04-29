d47a813b5708234eca72e5454b7d4dfff6ac32b2
Jesus Teixeira <jesusteixeira92@gmail.com>
2022-04-28 21:07:35 -0300

    Analisando Vendas por Data e Faixa Etária

--- Análise_Descritiva_SQL.sql
+++ Análise_Descritiva_SQL.sql
@@ -79,4 +79,42 @@ GROUP BY entrega_estado
 ORDER BY SUM(receita_bruta) DESC
 -- São paulo é o Estado com o maior faturamento e maior Número de pedidos --
 
---- Com base nas médias encontradas, poderia dizer que a persona dessa Loja é: Mulher de São Paulo que compra móveis até 3 vzs por ano. --
+-- Faixa Etária -- 
+SELECT SUM(receita_bruta) AS 'receita bruta', 
+	CASE
+		WHEN cliente_idade Between 0 And 20 Then '0 Até 20 Anos'
+		WHEN cliente_idade Between 21 And 40 Then '21 Até 40 Anos'
+		WHEN cliente_idade > 40 THEN 'Maior que 60'
+		ELSE 'outros' 
+	END AS "faixa_etaria"
+FROM Retail_Data_2018
+GROUP BY CASE
+		WHEN cliente_idade Between 0 And 20 Then '0 Até 20 Anos'
+		WHEN cliente_idade Between 21 And 40 Then '21 Até 40 Anos'
+		WHEN cliente_idade > 40 THEN 'Maior que 60'
+		ELSE 'outros' 
+	END 
+ORDER BY SUM(receita_bruta) DESC
+
+--- Com base nas médias encontradas, poderia dizer que a persona dessa Loja é: Mulher de São Paulo que tem entre 21 e 40 anos e compra móveis até 3 vzs por ano. --
+
+
+--- Analisando o Faturamento Ao longo do Ano -- 
+
+-- Faturamento Bruto Mensal --
+SELECT	SUM(receita_bruta) AS 'Faturamento' , DATEPART(MONTH, data_venda) AS Mês
+FROM Retail_Data_2018
+GROUP BY DATEPART(MONTH, data_venda)
+ORDER BY DATEPART(MONTH, data_venda) ASC 
+-- O ano tem uma tendência de alta ao longo do meses -- 
+-- Quão mais próximo do fim do ano, maior o faturamento, o pico de vendas ocorre em novembro.-- 
+
+-- Análise de Vendas por trimestre -- 
+SELECT	SUM(receita_bruta) AS 'Faturamento' , DATEPART(QUARTER, data_venda) AS Trimestre
+FROM Retail_Data_2018
+GROUP BY DATEPART(QUARTER, data_venda)
+ORDER BY DATEPART(QUARTER, data_venda) ASC 
+-- Como visto acima, o quarto trimestre é o que teve o maior faturamento -- 
+
+--- Com base na análise acima, sabe-se que o setor de móveis é o mais lucrativo, além disso as vendas aumentam ao longo do ano, principalmente no último trimestre -- 
+--- Black Friday e datas festivas são excelente pontos para se aproveitar, o perfil de cliente encontrado deve ser usado para ADS -- 
